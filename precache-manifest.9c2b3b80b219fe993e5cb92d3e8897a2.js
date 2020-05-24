1e430f2b5ef9f93fa51cf85baf53950ea53165e3
swaroop <swaroopanand325@gmail.com>
2020-05-25 01:56:25 +0530

    Updates

---
+++ precache-manifest.9c2b3b80b219fe993e5cb92d3e8897a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471d22dc7ec8fa0863170ae708e8d919",
+    "url": "/Favourite-Location-Tracker/index.html"
+  },
+  {
+    "revision": "97fa0273e5702db805aa",
+    "url": "/Favourite-Location-Tracker/static/css/main.46fd5e9e.chunk.css"
+  },
+  {
+    "revision": "903ed09227c7e74b1d43",
+    "url": "/Favourite-Location-Tracker/static/js/2.6d112058.chunk.js"
+  },
+  {
+    "revision": "b68b059067b674366ae428bb50e0ff89",
+    "url": "/Favourite-Location-Tracker/static/js/2.6d112058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97fa0273e5702db805aa",
+    "url": "/Favourite-Location-Tracker/static/js/main.75730f5a.chunk.js"
+  },
+  {
+    "revision": "2c1ce04e8589b2984cb6",
+    "url": "/Favourite-Location-Tracker/static/js/runtime-main.a320f5af.js"
+  }
+]);
